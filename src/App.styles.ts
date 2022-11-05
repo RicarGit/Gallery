e9fc00313b9a1fa8520c3e0c552adf1d09f0fdd3
Ricardo Gava <ricardoagava@gmail.com>
2022-11-04 22:18:02 -0300

    feat: getting all images from firebase and displaying on the screen
    if no images exists, a warning screen will shown and a loading screen will shown when load images

---
+++ src/App.styles.ts
@@ -0,0 +1,56 @@
+import styled from "styled-components/macro"
+
+export const Container = styled.div`
+  background-color: #27282f;
+  color: #fff;
+  min-height: 100vh;
+`
+
+export const Area = styled.div`
+  margin: auto;
+  max-width: 980px;
+  padding: 30px 0;
+`
+
+export const Heading = styled.h1`
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  margin-bottom: 30px;
+`
+
+export const ScreenWarning = styled.div`
+  text-align: center;
+
+  .emoji {
+    font-size: 50px;
+    margin-bottom: 20px;
+  }
+`
+
+export const PhotoList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+`
+
+export const UploadForm = styled.form`
+  background-color: #3D3F43;
+  padding: 15px;
+  border-radius: 10px;
+  margin-bottom: 30px;
+
+  input[type=submit] {
+    background-color: #756DF4;
+    border: none;
+    color: #fff;
+    padding: 8px 16px;
+    margin: 0 20px;
+    border-radius: 5px;
+    cursor: pointer;
+
+    &:hover {
+      opacity: .8;
+    }
+  }
+`
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as S from './App.styles'
+import * as Photos from './services/photos'
 
-function App() {
+import { useState, useEffect, FormEvent } from 'react'
+import { Photo } from './types/Photo'
+import { PhotoItem } from './components/PhotoItem'
+
+export function App() {
+  const [uploading, setUploading] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [photos, setPhotos] = useState<Photo[]>([])
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      setLoading(true)
+      setPhotos(await Photos.getAllPhotos())
+      setLoading(false)
+    }
+
+    getPhotos()
+  }, [])
+
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget)
+    const file = formData.get('image') as File
+
+    if (file && file.size > 0) {
+      setUploading(true)
+      let result = await Photos.insertImage(file)
+      setUploading(false)
+
+      if (result instanceof Error) {
+        alert(`${result.name} - ${result.message}`)
+      } else {
+        let newPhotoList = [...photos]
+        newPhotoList.push(result)
+        setPhotos(newPhotoList)
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <S.Container>
+      <S.Area>
+        <S.Heading>Galeria de fotos</S.Heading>
 
-export default App;
+        <S.UploadForm method='POST' onSubmit={handleFormSubmit}>
+          <input type='file' name='image' />
+          <input type='submit' value='Enviar' />
+          {uploading && 'Enviando...'}
+        </S.UploadForm>
+
+        {loading &&
+          <S.ScreenWarning>
+            <div className="emoji">😴</div>
+            <div>Carregando...</div>
+          </S.ScreenWarning>
+        }
+
+        {!loading && photos.length > 0 &&
+          <S.PhotoList>
+            {photos.map(({ url, name }, index) => (
+              <PhotoItem key={index} url={url} name={name} />
+            ))}
+          </S.PhotoList>
+        }
+
+        {!loading && photos.length === 0 &&
+          <S.ScreenWarning>
+            <div className="emoji">🤔</div>
+            <div>Não há fotos cadastradas...</div>
+          </S.ScreenWarning>
+        }
+      </S.Area>
+    </S.Container >
+  )
+}
