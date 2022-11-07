8561e57c7983c2ea6abad15622ee8f8a5ceebe2a
Ricardo Gava <ricardoagava@gmail.com>
2022-11-07 18:04:38 -0300

    feat: the file name is no more a random id, now the file name is limited to 30 characters

--- src/services/photos.ts
+++ src/services/photos.ts
@@ -1,7 +1,7 @@
 import { Photo } from "types/Photo"
 import { storage } from "libs/firebase"
 import { ref, listAll, getDownloadURL, uploadBytes } from "firebase/storage"
-import { v4 as createId } from 'uuid'
+// import { v4 as createId } from 'uuid'
 
 export const getAllPhotos = async () => {
   const list: Photo[] = []
@@ -22,9 +22,15 @@ export const getAllPhotos = async () => {
 }
 
 export const insertImage = async (file: File) => {
-  if (['image/jpeg', 'image/jpg', 'image/png'].includes(file.type)) {
-    let randomName = createId()
-    let newFile = ref(storage, `images/${randomName}`)
+  const [fileName] = file.name.split(/\.png$|\.jpg$|\.jpeg$/g)
+
+  if (fileName.length >= 30) {
+    return new Error('Nome do arquivo muito grande, máximo permitido de 30 caracteres')
+  }
+
+  if (file.type.match(/\/png$|\/jpg$|\/jpeg$/g)) {
+    // let randomName = createId()
+    let newFile = ref(storage, `images/${fileName}`)
 
     let upload = await uploadBytes(newFile, file)
     let photoURL = await getDownloadURL(upload.ref)
@@ -35,5 +41,5 @@ export const insertImage = async (file: File) => {
     } as Photo
   }
 
-  return new Error('Tipo de arquivo não permitido')
+  return new Error('Tipo de arquivo não permitido, somente arquivos jpg, jpeg ou png')
 }
