8febb42ea7894717ebc45b1409eb6824b9f12070
Ricardo Gava <ricardoagava@gmail.com>
2022-11-22 02:03:52 -0300

    refactor: created a handleDeleteImage function to refer it in the onClick property

--- src/components/TrashCan/index.tsx
+++ src/components/TrashCan/index.tsx
@@ -4,15 +4,23 @@ import deleteIcon from './delete.svg'
 import { api } from 'services/api'
 import { Card } from 'types/Card'
 
+
 export const TrashCan = ({ name, photos, setPhotos }: Omit<Card, 'url'>) => {
+  const handleDeleteImage = () => {
+    const filteredPhotos = photos.filter(photo => photo.name !== name)
+
+    api.deleteImage(name)
+    setPhotos(filteredPhotos)
+  }
+
   return (
     <S.TrashCan>
-      <img className='trashCan' src={deleteIcon} alt='trash icon' onClick={() => {
-        const filteredPhotos = photos.filter(photo => photo.name !== name)
-
-        api.deleteImage(name)
-        setPhotos(filteredPhotos)
-      }} >
+      <img
+        className='trashCan'
+        src={deleteIcon}
+        alt='trash icon'
+        onClick={handleDeleteImage}
+      >
       </img>
     </S.TrashCan>
   )
